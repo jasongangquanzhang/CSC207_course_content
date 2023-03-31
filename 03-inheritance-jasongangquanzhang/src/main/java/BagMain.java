636a8f1b8b7b90dcfb8e09010b982a00dae3c1ed
Gangquan Zhang <gangquan.zhang@mail.utoronto.ca>
2023-03-30 23:11:01 -0400

    Add files via upload

---
+++ 03-inheritance-jasongangquanzhang/src/main/java/BagMain.java
@@ -0,0 +1,50 @@
+/* This file contains a few exercises and TODOs for you to fill.
+ * Make sure you do the TODOs in Bag.java, HandBag.java and CrossbodyBag.java
+ * as the tasks in this file depends on the completion on those!
+ *
+ * You may find the readings in chapter 3. Relationships between Classes
+ * helpful while working through this exercise.
+ */
+class BagMain {
+    /**
+     * TODO: Implement this method
+     * Enhance each of the bags in bags. If double_enhance_handbags is
+     * True, then enhance any HandBags a second time.
+     * <p>
+     * This method should work for *all* Bags! We will test this method
+     * on new Bag types (and HandBag subclasses)!
+     */
+    public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
+        // TODO: Implement this.
+        for (Bag b : bags) {
+            b.enhance();
+        }
+        if (double_enhance_handbags) {
+            for (Bag b : bags) {
+                if (b instanceof HandBag) {
+                    b.enhance();
+                }
+            }
+        }
+    }
+
+    /**
+     * TODO: Implement this method
+     * Return the total number of straps in CrossbodyBags in bags.
+     * Any bag that is not a CrossbodyBag should not be included
+     * in the count!
+     *
+     * @param bags An array of Bags
+     * @return The total number of straps of CrossbodyBags.
+     */
+    public static int countCrossbodyStraps(Bag[] bags) {
+        // TODO: Implement this.
+        int result = 0;
+        for (Bag b : bags) {
+            if (b instanceof CrossbodyBag) {
+                result += ((CrossbodyBag) b).getNumberOfStraps();
+            }
+        }
+        return result;
+    }
+}
---
+++ 04-assorted-topics-in-java-jasongangquanzhang/src/main/java/DrivableTrader.java
@@ -0,0 +1,37 @@
+/* TODO: Create a subclass of Trader named DrivableTrader
+ * This class should be identical to Trader, except that it takes
+ * only Drivable objects in its inventory, wishlist, etc.
+ *
+ * The sellingPrice returned should also be overridden. The selling price
+ * should be equal to the:
+ *     Object's price + Object's max speed
+ * If the object is Tradable (and Tradable.MISSING_PRICE otherwise.)
+ *
+ * Look at DomesticatableTrader.java for an example.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+class DrivableTrader extends Trader<Drivable> {
+
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    @Override
+    public int getSellingPrice(Drivable item) {
+
+        int p = super.getSellingPrice(item);
+        if (p == Tradable.MISSING_PRICE) {
+            return p;
+        } else {
+            return p + item.getMaxSpeed();
+        }
+    }
+
+}
